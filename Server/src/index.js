e4405437b306c2480461c2cbaae3e315c0299583
Ishant-179 <rajishant128@gmail.com>
2025-08-10 00:21:18 +0530

    cors origin changed

--- Server/src/index.js
+++ Server/src/index.js
@@ -21,15 +21,23 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 
-app.use(
-  cors({
-    origin: [
+const allowedOrigins = [
   "https://chat-app-pkoz.onrender.com",
-   "http://localhost:5174"
-],
-    credentials: true,
-  })
-);
+  "http://localhost:5174"
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin (like mobile apps, curl)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
