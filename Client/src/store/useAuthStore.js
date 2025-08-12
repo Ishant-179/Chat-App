f8b0fd9ebc713ffb43cf4e28eec49f091dfd0b84
Ishant-179 <rajishant128@gmail.com>
2025-08-12 12:55:55 +0530

    enviroment variable updated

--- Client/src/store/useAuthStore.js
+++ Client/src/store/useAuthStore.js
@@ -3,7 +3,9 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const SOCKET_BASE_URL = import.meta.env.VITE_BACKEND_URL
+  ? import.meta.env.VITE_BACKEND_URL.replace(/\/api$/, "") // remove trailing /api for socket url
+  : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -86,7 +88,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
+    const socket = io(SOCKET_BASE_URL, {
       query: {
         userId: authUser._id,
       },
